89a734cc90a7d76eb6debc4ed843dad875ed08e2
radenlmantuano <newcodeondablock@gmail.com>
2021-09-04 12:37:45 -0700

    Learning_Dashboard (initial commit)

---
+++ server.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, session, redirect,request
+
+app = Flask (__name__)
+app.secret_key = 'secret key'
+
+@app.route('/')
+def index():
+    return render_template ('index.html')
+
+@app.route('/login', methods=['Post'])
+def login():
+    session['email'] = request.form['email']    
+    session['password'] = request.form['password']
+    return redirect('/user')
+
+
+@app.route('/submit', methods=['Post'])
+def createAccount():
+    session['full_name'] = request.form['full_name']
+    session['email'] = request.form['email']    
+    session['confirm_email'] = request.form['confirm_email']
+    session['password'] = request.form['password']
+    return redirect ('/user')
+
+@app.route('/user')
+def userDash():
+    return render_template('/user.html', full_name = session['full_name'], email = session['email'], confirm_email = session['confirm_email'], password = session['password'])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,25 @@
+var clickedBookMark = true;
+
+function bookMark (element){
+
+    if(clickedBookMark){
+
+        element.src="static/images/bookmark _filled.png";
+        clickedBookMark = false;
+    }
+    else {
+
+        element.src="static/images/bookmark.png";
+        clickedBookMark = true;
+    }
+}
+
+let popUp = document.querySelector('.message-slider');
+
+function messagePop(){
+
+    popUp.classList.add('active');
+
+}
+
+
